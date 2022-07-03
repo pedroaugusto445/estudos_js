17fbafeaad425e20fc0dbed6105660e020ff876f
pedroaugusto445 <magodoscodicos@gmail.com>
2022-07-03 14:02:19 -0300

    ww

---
+++ ex005/js.js
@@ -0,0 +1,9 @@
+var imagem = document.createElement("img")
+
+imagem.src = "js.png"
+
+var palavra = document.createElement("p")
+palavra.innerHTML = "ola mundo"
+console.log(
+    imagem, palavra
+)
---
+++ ex006/js.js
@@ -0,0 +1,9 @@
+var p = document.createElement("p")
+p.innerHTML = "fica comigo deus"
+
+var img = document.createElement("img")
+img.src = "../ex005/js.png"
+
+
+document.getElementById("conteudo").appendChild(p)
+document.getElementById("conteudo").appendChild(img)
