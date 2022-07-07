2e2ddcf35c22284326bf2d56d95d18bcceab565c
pedroaugusto445 <magodoscodicos@gmail.com>
2022-07-07 10:59:33 -0300

    commit

---
+++ ex014/js.js
@@ -0,0 +1,14 @@
+
+
+let pais = "canada"
+let estado = "bahia"
+
+switch(pais) {
+    case 'brasil':
+        console.log('eeeee brasillllllll')
+        break
+
+        case 'canada':
+            console.log('aaa canada bobao')
+            break
+}
---
+++ ex015/js.js
@@ -0,0 +1,20 @@
+let fruta = {
+    nome: "banana",
+    preco: 9.99,
+    unidade: 1,
+}
+
+let aparelhos = ["vone","celular","ps4","pc"]
+
+/*
+for (let x in fruta) {
+
+
+
+}
+
+*/
+
+for(let x of aparelhos) {
+    
+}
